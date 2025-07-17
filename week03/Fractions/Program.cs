f35ed360888108ccb246bf8191fbb76ca1c2e0b2
Laurelpere <pelau360@gmail.com>
2025-07-17 06:11:19 +0100

    Fraction solution

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction frac = new Fraction();
+        frac.SetTop(1);
+        Fraction frac2 = new Fraction();
+        frac2.SetTop(6);
+        Fraction frac3 = new Fraction();
+        frac3.SetTop(3);
+        frac3.SetBottom(4);
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac.GetTop());
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetTop());
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac3.GetFractionString());  
+        Console.WriteLine(frac3.GetDecimalValue());  
+       
+
     }
 }
