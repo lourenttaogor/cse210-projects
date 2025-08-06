0783c0100d5de7a61d039fc4906fbb387a9abd5d
Laurelpere <pelau360@gmail.com>
2025-08-06 15:26:56 +0100

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square(6, "blue");
+        
+        // Corrected constructor call with two arguments
+        Rectangle rectangle = new Rectangle(8, 4, "White");
+        
+        // Corrected constructor call with two arguments
+        Circle circle = new Circle(12.5, "purple");
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string shapeColor = shape.GetColor();
+            Console.WriteLine(area);
+             Console.WriteLine(shapeColor);
+        }
     }
 }
